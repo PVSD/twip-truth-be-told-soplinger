32657b8ff082e26e57734bfe2f9a040890b0ae0e
soplinger <so367@pvsd.org>
2018-12-10 11:21:14 -0500

    asdasdasd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
-
+import java.io.*;
+import java.util.*;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        FileWriter writer = new FileWriter("the-file-name.txt");
+        String aaaa = "pls addada";
+        writer.write(aaaa);
+        System.out.println("A\t" + "B\t" + "!(A&&B)" );
+        System.out.println("T\t" + "T\t" + "F\t");
+        writer.close();
+        boolean a = true;
+        boolean b = false;
+        System.out.println(!(a&&b));
+
     }
 }
